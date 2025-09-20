6fb3b3217f75763e2df278589e9f057f01b52ad8
wangzhx <1132154046@qq.com>
2025-09-20 16:23:58 +0800

    debug

--- fixtures/packaging/vite/src/main.tsx
+++ fixtures/packaging/vite/src/main.tsx
@@ -3,4 +3,14 @@ import './style.css'
 console.log("react is ", React)
 console.log("react-dom is ", ReactDOM)
 
-ReactDOM.createRoot(document.getElementById("app"))
+const root = ReactDOM.createRoot(document.getElementById("app"))
+
+class ClassComponent extends React.Component {
+  render() {
+    const { initialCount } = this.props;
+
+    return <p>Class Count is: {initialCount}</p>;
+  }
+}
+
+root.render(<div>hello</div>)
